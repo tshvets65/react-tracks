738e68494d4635e1af6a4394ce1d8dda8a7bbdbf
Tetyana Shvets <tshvets@hotmail.com>
2019-03-21 11:15:33 -0700

    Signout component

--- react-tracks-client/src/Root.js
+++ react-tracks-client/src/Root.js
@@ -1,15 +1,29 @@
 import React from "react";
 import { gql } from 'apollo-boost';
 import { Query } from 'react-apollo';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import withRoot from "./withRoot";
+import App from './pages/App'
+import Profile from './pages/Profile'
+import Header from './components/Shared/Header'
 
 const Root = () => (
     <Query query={ME_QUERY}>
         {({ data, loading, error }) => {
             if (loading) return <p>Loading...</p>
             if (error) return <p>Error {error.message}</p>
-
-            return <p>{JSON.stringify(data)}</p>
+            const currentUser = data.me
+            return (
+                <BrowserRouter>
+                    <>
+                        <Header currentUser={currentUser} />
+                        <Switch>
+                            <Route exact path='/' component={App} />
+                            <Route path='/profile/:id' component={Profile} />
+                        </Switch>
+                    </>
+                </BrowserRouter>
+            )
         }}
     </Query>
 )
--- react-tracks-client/src/components/Auth/Signout.js
+++ react-tracks-client/src/components/Auth/Signout.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { ApolloConsumer } from 'react-apollo'
 import withStyles from "@material-ui/core/styles/withStyles";
-// import ExitToApp from "@material-ui/icons/ExitToApp";
-// import Typography from "@material-ui/core/Typography";
-// import Button from "@material-ui/core/Button";
+import ExitToApp from "@material-ui/icons/ExitToApp";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
 const Signout = ({ classes }) => {
-  return <div>Signout</div>;
+  const handleSignout = client => {
+    localStorage.removeItem('authToken')
+    client.writeData({ data: { isLoggedIn: false } }) // can also use client.resetState()
+  }
+
+  return (
+    <ApolloConsumer>
+      {client => (
+        <Button onClick={() => handleSignout(client)}>
+          <Typography variant='body1' className={classes.buttonText} color='secondary'>
+            Sign out
+          </Typography>
+          <ExitToApp className={classes.buttonIcon} color='secondary' />
+        </Button>
+      )}
+    </ApolloConsumer>
+  )
 };
 
 const styles = {
